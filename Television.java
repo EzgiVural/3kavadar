75dd4c9c9173bf8c90f8744ae5c5144273c5e244
melikeKucukkurutlu <33579857+melikeKucukkurutlu@users.noreply.github.com>
2017-11-12 01:08:56 +0300

    Add files via upload

---
+++ Television.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proje;
+
+/**
+ *
+ * @author Melike
+ */
+
+public class Television extends Product{
+    
+    public Television (int pid, String brand, String model)
+    {
+        super();
+     
+        
+    }
+    
+}
